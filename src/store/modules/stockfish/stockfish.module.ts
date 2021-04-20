a1e48b148dfe84460513b0e52adc1966f2197715
Colin Faivre <colin.faivre@gmail.com>
2021-04-20 14:49:25 +0200

    Reorganize stockfish module

--- src/store/modules/stockfish/stockfish.module.ts
+++ src/store/modules/stockfish/stockfish.module.ts
@@ -1,11 +1,10 @@
 import { VuexModule, Module, Mutation, Action, getModule } from 'vuex-module-decorators';
 import {BoardModule} from '@/store/modules';
 import store from '@/store';
+import { IStockfishState } from '@/store/modules/stockfish/stockfish';
+import stockfishMutations from '@/store/modules/stockfish/stockfish.mutations';
 
 const stockfishWorker = new Worker('stockfish.js/stockfish.js', {type: 'module'});
-
-import { IStockfishState } from '@/store/modules/stockfish/stockfish.d.ts';
-import stockfishMutations from '@/store/modules/stockfish/stockfish.mutations';
 
 @Module({
     dynamic: true,
@@ -21,33 +20,31 @@ class Stockfish extends VuexModule implements IStockfishState{
     }
 
     @Action({ rawError: true })
+    public init() {
+        stockfishWorker.postMessage('uci');
+        stockfishWorker.postMessage('ucinewgame');
+        
+        stockfishWorker.onmessage = function(e) {
+            console.log(e.data);
+        }
+    }
+    
+    @Action({ rawError: true })
+    public setComputerLevel(level: number) {
+        this.context.commit(stockfishMutations.SET_COMPUTER_LEVEL, level);
+    }
+
+    @Action({ rawError: true })
     public guessNextMove(movesAsString: string) {
         console.log('moves', movesAsString);
         stockfishWorker.postMessage(`position startpos moves ${movesAsString}`);
         stockfishWorker.postMessage(`go movetime ${this.computerLevel * 1000}`);
-
-        const context = this.context;
 
         stockfishWorker.onmessage = function(e) {
             if (e.data.split(' ')[0] === 'bestmove') {
                 BoardModule.stockfishPlays(e.data.split(' ')[1])
             }
         }
-    }
-
-    @Action({ rawError: true })
-    public init() {
-        stockfishWorker.postMessage('uci');
-        stockfishWorker.postMessage('ucinewgame');
-
-        stockfishWorker.onmessage = function(e) {
-            console.log(e.data);
-        }
-    }
-
-    @Action({ rawError: true })
-    public setComputerLevel(level: number) {
-        this.context.commit(stockfishMutations.SET_COMPUTER_LEVEL, level);
     }
 }
 
